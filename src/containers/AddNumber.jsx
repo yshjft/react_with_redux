311b6a146e5b0571a30617eadb09694c871e0481
yshjft <jericoh@naver.com>
2020-08-14 03:05:08 +0900

    mapStateProps & mapDispatchToProps

--- src/containers/AddNumber.jsx
+++ src/containers/AddNumber.jsx
@@ -1,9 +1,20 @@
 import AddNumber from "../components/AddNumber";
 import {connect} from "react-redux";
-export default connect()(AddNumber);
+
+// mapDispatchToProps
+// 1번째 인자로 store.dispatch라는 API를 공급
+function mapReduxDispatchToReactProps(dispatch){
+    return {
+        onClick:function(size){
+            dispatch({type : "INCREMENT", size:size});
+        }
+    }
+}
+
+export default connect(null, mapReduxDispatchToReactProps)(AddNumber);
+
 
 /* without react-redux
-
 container component
 컴포넌트가 redux에 종속되는 것을 방지하기위해 사용한다.
 컴포넌트가 redux에 종속될 경우 재사용이 어렵다.
--- src/containers/DisplayNumber.jsx
+++ src/containers/DisplayNumber.jsx
@@ -1,11 +1,25 @@
 import DisplayNumber from "../components/DisplayNumber";
 import {connect} from 'react-redux';
 
+// mapStateToProps
+// redux의 store 값이 바뀔 때마다 호출된다(subscribe 역할)
+// state == redux의 state
+/*
+return{
+    (전달될 props 이름) : (공급될 값)
+}
+*/
+function mapReduxStateTReactoProps(state){
+    return {
+        number: state.number
+    }
+}
+
 // connect()의 return 값은 함수이다.
 // return된 함수를 통해 만들어진 값을 export 한다.
 // 최종적으로 wrapping된 컴포넌트가 return된다.
 // props 또한 자동 전달한다.
-export default connect()(DisplayNumber);
+export default connect(mapReduxStateTReactoProps)(DisplayNumber);
 
 
 /* without react-redux
